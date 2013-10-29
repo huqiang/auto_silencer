e2dd124b9a759bbf1cd9121123fb62efb66d54b0
zi xian <zixian1988@gmail.com>
2013-10-29 14:33:55 +0800

    done with button.

--- src/sg/edu/nus/cs4274/autosilencer/MainActivity.java
+++ src/sg/edu/nus/cs4274/autosilencer/MainActivity.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.cs4274.autosilencer;
 
 import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
@@ -9,14 +8,20 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	WifiManager wifi;
 	List<ScanResult> results;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 
 		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
@@ -40,14 +45,34 @@ public class MainActivity extends Activity {
 				audiomanage.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 			}
 		}
-	}
-	
-	@Override
-	public void onDestroy() {
-	    super.onDestroy();  // Always call the superclass
-	    
-	    // Stop method tracing that the activity started during onCreate()
-	    android.os.Debug.stopMethodTracing();
-	}
 
+		setContentView(R.layout.activity_main);
+	
+
+
+	Button cb=(Button) findViewById(R.id.button1);
+
+	cb.setOnClickListener(new View.OnClickListener()
+	{
+
+	@Override
+	public void onClick(View v)
+	{
+		Context context = getApplicationContext();
+		CharSequence text = "Hello toast!";
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+
+	}
+	});
+	
+	
+	
+	
+	
+
+	
+	}
 }
