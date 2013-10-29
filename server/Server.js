5e5f1aa7ac5acb29db91e10fe26f4d52f9c685ab
Hu Qiang <nickhuq@gmail.com>
2013-10-29 15:16:25 +0800

    Added server with hardcoded router timing.. please install express as : 		npm install express run server: 		node Server.js

---
+++ server/Server.js
@@ -0,0 +1,39 @@
+
+var port       = 4274;
+var express    = require("express");
+var http       = require('http');
+var sys        = require('sys')
+var app        = express();
+//var httpServer = http.createServer(app);
+//httpServer.listen(port, "0.0.0.0");
+var routers=[];
+routers[0] = {"name":"CS4274_01", "slientTime": [{"start":1300, "end":1500}]};
+routers[1] = {"name":"CS4274_02", "slientTime": [{"start":1300, "end":1500},{"start":1600, "end":1630}]};
+//routers["CS4274_01"] = [{"start":1300, "end":1500}];
+//routers["CS4274_02"] = [{"start":1300, "end":1500},{"start":1600, "end":1630}];
+var response;
+
+app.use(express.static(__dirname));
+
+    
+app.get("/", function(req, res) {
+  res.send(JSON.stringify(routers));
+});
+
+app.get("/:rID", function(req, res) {
+	var id = req.params.rID;
+  res.send(JSON.stringify(routers[id]));
+});
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + req.params[0]); 
+ });
+
+ app.listen(port, function() {
+    console.log("Listening on " + port);
+  });
+
+
+
