581c3c29f8f42314504c6785b812526782c1a29e
zi xian <zixian1988@gmail.com>
2013-11-11 17:57:07 +0800

    Full working copy with Schdule and Display

--- server/Server.js
+++ server/Server.js
@@ -11,8 +11,8 @@ var routers={};
 //routers[1] = {"name":"CS4274_02", "slientTime": [{"start":1300, "end":1500},{"start":1600, "end":1630}]};
 routers["CS4274_01"] = [{"title":"CS4274_01_Event_One","startHour":13, "startMinute":0, "endHour":15, "endMinute":0}];
 routers["CS4274_02"] = [{"title":"CS4274_02_Event_One","startHour":13, "startMinute":0, "endHour":15, "endMinute":0},{"title":"CS4274_02_Event_Two","startHour":16, "startMinute":0, "endHour":16, "endMinute":30}];
-routers["NUS"] = [{"title":"NUS_TEST","startHour":18, "startMinute":10, "endHour":18, "endMinute":11}];
-routers["NUS"] = [{"title":"NUS_TEST","startHour":16, "startMinute":10, "endHour":17, "endMinute":11}];
+routers["NUS"] = [{"title":"NUS_TEST","startHour":18, "startMinute":10, "endHour":18, "endMinute":11}, {"title":"NUS_TEST2","startHour":16, "startMinute":30, "endHour":16, "endMinute":50}];
+//routers["NUS"] = [{"title":"NUS_TEST","startHour":16, "startMinute":10, "endHour":17, "endMinute":11}];
 //,{"title":"CS4274_02_Event_Two","startHour":16, "startMinute":0, "endHour":16, "endMinute":30}];
 var response;
 
